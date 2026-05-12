92eb9ea79ddc4a3c8aab5aacb99b5e78cbe05b0b
JulianGs93 <jcguerrerosa@unadvirtual.edu.co>
2026-05-12 00:02:06 -0500

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,16 @@
+import logging
 
+
+def configurar_logger():
+    """
+    Configura el sistema de logs del proyecto.
+    Los mensajes se guardan en el archivo logs.txt.
+    """
+    logging.basicConfig(
+        filename="logs.txt",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        encoding="utf-8"
+    )
+
+    return logging.getLogger("SoftwareFJ")
