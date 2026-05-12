2d880e51b875d50872ad9ddccad424fb3b3b77b2
JulianGs93 <jcguerrerosa@unadvirtual.edu.co>
2026-05-12 00:02:48 -0500

    Update entidad.py

--- entidad.py
+++ entidad.py
@@ -1,1 +1,14 @@
+from abc import ABC, abstractmethod
 
+
+class Entidad(ABC):
+    """
+    Clase abstracta base para todas las entidades del sistema.
+    """
+
+    @abstractmethod
+    def mostrar_informacion(self):
+        """
+        Método abstracto que debe ser implementado por las clases hijas.
+        """
+        pass
